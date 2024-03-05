20eca0e2e56edececeb0e0c46100533eeab42621
David Veytsman <david.veytsman@gmail.com>
2024-03-05 13:59:42 -0500

    - added ball bounce logic (off the walls)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 
 screen = Screen()
@@ -12,7 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-
+game_ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.up, "i")
@@ -24,5 +26,14 @@ screen.onkey(l_paddle.down, "s")
 game_on = True
 while game_on:
     screen.update()
-    
+    time.sleep(.05)
+    game_ball.move()
+
+    # Detect collision
+    if game_ball.ycor() > 280 or game_ball.ycor() < -280:
+        game_ball.bounce()
+
+    if game_ball.xcor() > 380 or game_ball.xcor() < -380:
+        game_ball.hit()
+
 screen.exitonclick()
